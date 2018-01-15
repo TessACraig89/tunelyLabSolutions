9fb3a492c43c20836ff6c4cbb2241324bb800d57
Tess Craig <tessashleycraig89@gmail.com>
2018-01-14 19:03:38 -0600

    album songs show

--- models/index.js
+++ models/index.js
@@ -1,10 +1,7 @@
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/tunely_test");
-
-//require Album S1S3 TC
-var Album = require('./album'); 
+mongoose.connect("mongodb://localhost/tunely");
+var Album = require('./album');
 var Song = require('./song');
 
-//export Album S1S3 TC
-module.exports.Album = Album; S1S3
+module.exports.Album = Album;
 module.exports.Song = Song;
